621f1eca49d5d6fa6f4433d73ae69ab7d829c941
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:26:20 +0530

    refactor(ruby): load execution policy explicitly

--- ruby/lib/agentic_query.rb
+++ ruby/lib/agentic_query.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require "set"
+require_relative "agentic_query/execution_policy"
 require_relative "agentic_query/policy"
 require_relative "agentic_query/row_constraint"
 require_relative "agentic_query/active_record"
 require_relative "agentic_query/schema"
 
 module AgenticQuery
-  VERSION = "0.1.1"
+  VERSION = "0.1.0"
 
   class QueryValidationError < StandardError; end
 
