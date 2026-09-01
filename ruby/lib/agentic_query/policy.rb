8149058d05dfaf23e87da63b249437d6e010d32b
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:20:56 +0530

    feat(ruby): add deterministic query policy engine

---
+++ ruby/lib/agentic_query/policy.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module AgenticQuery
+  # Deterministic authorization and resource policy for model-generated queries.
+  # Policies are application code and are never delegated to the model.
+  class Policy
+    attr_reader :max_rows, :timeout_ms
+
+    def initialize(max_rows: 1_000, timeout_ms: 5_000)
+      @max_rows = max_rows
+      @timeout_ms = timeout_ms
+      @allowed_entities = nil
+      @denied_fields = Hash.new { |hash, key| hash[key] = [] }
+    end
+
+    def allow_entities(*entities)
+      @allowed_entities = entities.flatten.map(&:to_s).to_set
+    end
+
+    def deny_fields(entity, *fields)
+      @denied_fields[entity.to_s].concat(fields.flatten.map(&:to_s)).uniq!
+    end
+
+    def authorize!(query)
+      validate_limit!(query)
+      authorize_entities!(query)
+      authorize_fields!(query)
+      query
+    end
+
+    def entity_allowed?(entity)
+      @allowed_entities.nil? || @allowed_entities.include?(entity.to_s)
+    end
+
+    def field_allowed?(entity, field)
+      !@denied_fields.fetch(entity.to_s, []).include?(field.to_s)
+    end
+
+    private
+
+    def validate_limit!(query)
+      limit = query["limit"]
+      return if limit.nil? || limit.to_i <= @max_rows
+
+      raise QueryValidationError, "Query limit exceeds policy maximum"
+    end
+
+    def authorize_entities!(query)
+      entities = [query.fetch("source").fetch("name")]
+      entities.concat(Array(query["joins"]).map { |join| join.fetch("entity").fetch("name") })
+
+      entities.each do |entity|
+        next if entity_allowed?(entity)
+
+        raise QueryValidationError, "Entity is not allowed: #{entity}"
+      end
+    end
+
+    def authorize_fields!(query)
+      expressions = Array(query["select"])
+      expressions.concat(Array(query["groupBy"]).map { |field| { "field" => field } })
+      expressions.concat(Array(query["orderBy"]).map { |order| { "field" => order.fetch("field") } })
+
+      expressions.each do |expression|
+        field = expression.fetch("field")
+        entity = field["entity"] || query.fetch("source").fetch("name")
+        name = field.fetch("field")
+
+        next if field_allowed?(entity, name)
+
+        raise QueryValidationError, "Field is not allowed: #{entity}.#{name}"
+      end
+    end
+  end
+end
