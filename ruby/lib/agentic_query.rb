b8847da5a1403c84199702cfc747745f8ec3342a
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:27:31 +0530

    refactor(ruby): load execution boundary

--- ruby/lib/agentic_query.rb
+++ ruby/lib/agentic_query.rb
@@ -6,17 +6,20 @@ require_relative "agentic_query/policy"
 require_relative "agentic_query/row_constraint"
 require_relative "agentic_query/active_record"
 require_relative "agentic_query/schema"
+require_relative "agentic_query/executor"
 
 module AgenticQuery
   VERSION = "0.1.0"
 
   class QueryValidationError < StandardError; end
+  class QueryExecutionError < StandardError; end
 
   module QueryValidator
     module_function
 
     def validate!(query, policy = {})
-      Policy.new(**policy).authorize!(query)
+      policy = policy.is_a?(Policy) ? policy : Policy.new(**policy)
+      policy.authorize!(query)
       query
     end
   end
