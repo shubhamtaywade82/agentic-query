bd73b871a4270ae80022bcb6793c1b08d6598796
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:21:10 +0530

    refactor(ruby): expose policy and ActiveRecord adapter

--- ruby/lib/agentic_query.rb
+++ ruby/lib/agentic_query.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
+
+require "set"
+require_relative "agentic_query/policy"
+require_relative "agentic_query/active_record"
+
 module AgenticQuery
-  VERSION = '0.1.0'
+  VERSION = "0.1.0"
+
   class QueryValidationError < StandardError; end
+
   module QueryValidator
     module_function
+
     def validate!(query, policy = {})
-      select = query.fetch('select')
-      raise QueryValidationError, 'Query must select at least one expression' if select.empty?
-      if policy[:max_rows] && query['limit'] && query['limit'] > policy[:max_rows]
-        raise QueryValidationError, 'Query limit exceeds policy maximum'
+      select = query.fetch("select")
+      raise QueryValidationError, "Query must select at least one expression" if select.empty?
+
+      if policy[:max_rows] && query["limit"] && query["limit"] > policy[:max_rows]
+        raise QueryValidationError, "Query limit exceeds policy maximum"
       end
-      if policy[:allowed_entities]
-        entities = [query.fetch('source').fetch('name'), *Array(query['joins']).map { |join| join.fetch('entity').fetch('name') }]
-        entities.each { |entity| raise QueryValidationError, "Entity is not allowed: #{entity}" unless policy[:allowed_entities].include?(entity) }
+
+      allowed = policy[:allowed_entities]
+      if allowed
+        entities = [query.fetch("source").fetch("name"), *Array(query["joins"]).map { |join| join.fetch("entity").fetch("name") }]
+        entities.each do |entity|
+          raise QueryValidationError, "Entity is not allowed: #{entity}" unless allowed.include?(entity)
+        end
       end
+
       query
     end
   end
