8a5298991b2e858cec817f2b4d3e501081fb4b66
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:21:51 +0530

    fix(ruby): compile filters and aggregates through Arel

--- ruby/lib/agentic_query/active_record.rb
+++ ruby/lib/agentic_query/active_record.rb
@@ -8,14 +8,14 @@ module AgenticQuery
   # Entity names are resolved only through an explicit registry.
   class ActiveRecordAdapter
     AGGREGATES = {
-      "count" => :count,
-      "sum" => :sum,
-      "avg" => :average,
-      "min" => :minimum,
-      "max" => :maximum
+      "count" => "COUNT",
+      "sum" => "SUM",
+      "avg" => "AVG",
+      "min" => "MIN",
+      "max" => "MAX"
     }.freeze
 
-    OPERATORS = {
+    PREDICATES = {
       "eq" => :eq,
       "neq" => :not_eq,
       "gt" => :gt,
@@ -39,7 +39,8 @@ module AgenticQuery
       relation = apply_joins(relation, query)
       relation = apply_filters(relation, query)
       relation = apply_select(relation, query)
-      relation = relation.group(*group_columns(query)) unless group_columns(query).empty?
+      groups = group_columns(query, model)
+      relation = relation.group(*groups) unless groups.empty?
       relation = apply_order(relation, query)
       relation = relation.limit([query["limit"] || policy.max_rows, policy.max_rows].min)
       relation = relation.offset(query["offset"]) if query["offset"]
@@ -51,7 +52,6 @@ module AgenticQuery
     def apply_joins(relation, query)
       Array(query["joins"]).each do |join|
         entity = join.fetch("entity").fetch("name").to_s
-        # Prefer explicit ActiveRecord association names. Raw join SQL is never accepted.
         association = relation.klass.reflect_on_all_associations.find { |reflection| reflection.name.to_s == entity }
         raise QueryValidationError, "Join is not a registered association: #{entity}" unless association
 
@@ -61,30 +61,31 @@ module AgenticQuery
     end
 
     def apply_filters(relation, query)
+      table = relation.klass.arel_table
+
       Array(query["filters"]).each do |filter|
         field = filter.fetch("field")
         name = field.fetch("field").to_s
         validate_column!(relation.klass, name)
         operator = filter.fetch("operator").to_s
+        column = table[name]
 
-        if operator == "is_null"
-          relation = relation.where(name => nil)
-        elsif operator == "is_not_null"
-          relation = relation.where.not(name => nil)
-        elsif operator == "in" || operator == "not_in"
-          values = filter.fetch("values")
-          relation = operator == "in" ? relation.where(name => values) : relation.where.not(name => values)
-        elsif operator == "between"
-          values = filter.fetch("values")
-          raise QueryValidationError, "between requires exactly two values" unless values.length == 2
-          relation = relation.where(name => values[0]..values[1])
-        elsif OPERATORS.key?(operator)
-          relation = relation.where(name => { OPERATORS.fetch(operator) => filter.fetch("value") })
-        elsif operator == "like"
-          relation = relation.where(relation.klass.arel_table[name].matches(filter.fetch("value")))
-        else
-          raise QueryValidationError, "Unsupported filter operator: #{operator}"
-        end
+        predicate = case operator
+                    when "is_null" then column.eq(nil)
+                    when "is_not_null" then column.not_eq(nil)
+                    when "in" then column.in(filter.fetch("values"))
+                    when "not_in" then column.not_in(filter.fetch("values"))
+                    when "between"
+                      values = filter.fetch("values")
+                      raise QueryValidationError, "between requires exactly two values" unless values.length == 2
+                      column.between(values[0]..values[1])
+                    when "like" then column.matches(filter.fetch("value"))
+                    when *PREDICATES.keys then column.public_send(PREDICATES.fetch(operator), filter.fetch("value"))
+                    else
+                      raise QueryValidationError, "Unsupported filter operator: #{operator}"
+                    end
+
+        relation = relation.where(predicate)
       end
       relation
     end
@@ -99,7 +100,7 @@ module AgenticQuery
         aggregate = expression["aggregate"]
         if aggregate
           function = AGGREGATES.fetch(aggregate.to_s) { raise QueryValidationError, "Unsupported aggregate: #{aggregate}" }
-          node = column.public_send(function)
+          node = Arel::Nodes::NamedFunction.new(function, [column])
           expression["alias"] ? node.as(expression["alias"].to_s) : node
         else
           expression["alias"] ? column.as(expression["alias"].to_s) : column
@@ -109,11 +110,11 @@ module AgenticQuery
       relation.select(*expressions)
     end
 
-    def group_columns(query)
+    def group_columns(query, model)
       Array(query["groupBy"]).map do |field|
         name = field.fetch("field").to_s
-        validate_column!(@models.fetch(query.fetch("source").fetch("name").to_s), name)
-        name
+        validate_column!(model, name)
+        model.arel_table[name]
       end
     end
 
