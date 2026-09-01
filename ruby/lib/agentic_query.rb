78662d6e20bcd4cf1b670db86ab9ae14cc60c605
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 07:22:53 +0530

    refactor(ruby): centralize query validation

--- ruby/lib/agentic_query.rb
+++ ruby/lib/agentic_query.rb
@@ -3,6 +3,7 @@
 require "set"
 require_relative "agentic_query/policy"
 require_relative "agentic_query/active_record"
+require_relative "agentic_query/schema"
 
 module AgenticQuery
   VERSION = "0.1.0"
@@ -13,21 +14,7 @@ module AgenticQuery
     module_function
 
     def validate!(query, policy = {})
-      select = query.fetch("select")
-      raise QueryValidationError, "Query must select at least one expression" if select.empty?
-
-      if policy[:max_rows] && query["limit"] && query["limit"] > policy[:max_rows]
-        raise QueryValidationError, "Query limit exceeds policy maximum"
-      end
-
-      allowed = policy[:allowed_entities]
-      if allowed
-        entities = [query.fetch("source").fetch("name"), *Array(query["joins"]).map { |join| join.fetch("entity").fetch("name") }]
-        entities.each do |entity|
-          raise QueryValidationError, "Entity is not allowed: #{entity}" unless allowed.include?(entity)
-        end
-      end
-
+      Policy.new(**policy).authorize!(query)
       query
     end
   end
