16fe26462dae6a9ab726327ef0aab13058ce04fb
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-01 10:02:48 +0530

    feat(ruby): export postgres adapter

--- ruby/lib/agentic_query.rb
+++ ruby/lib/agentic_query.rb
@@ -4,12 +4,14 @@ require "set"
 require_relative "agentic_query/execution_policy"
 require_relative "agentic_query/policy"
 require_relative "agentic_query/row_constraint"
+require_relative "agentic_query/tenant_scope"
 require_relative "agentic_query/active_record"
+require_relative "agentic_query/postgresql_adapter"
 require_relative "agentic_query/schema"
 require_relative "agentic_query/executor"
 
 module AgenticQuery
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   class QueryValidationError < StandardError; end
   class QueryExecutionError < StandardError; end
